23564be05a2280fd0357269084baa74610366b88
Ayyappa-Naga <134542493+Ayyappa-Naga@users.noreply.github.com>
2023-10-04 13:34:30 +0530

    Update MavenPractic.java

--- src/test/java/com/testPractice/MavenPractic.java
+++ src/test/java/com/testPractice/MavenPractic.java
@@ -10,11 +10,6 @@ import org.testng.annotations.Test;
 		@Test(groups="smoke")
 		public void method()
 		{
-			System.out.print("hello");
-			System.out.print("hello");
-			System.out.print("hello");
-			System.out.print("hello");
-			System.out.print("hello");
 		}
 	@Test(groups = "system")
 		public void method1()
